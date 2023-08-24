d915c6b977eb96eb6d6b459b398192518a747d1f
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-08-24 16:54:40 -0400

    separates out editor from folders

--- apps/create/widget/editor/index.jsx
+++ apps/create/widget/editor/index.jsx
@@ -13,9 +13,143 @@ const on = {
   publish: () => handle["document"].publish(projectId, path),
 };
 
-return widget("/*__@appAccount__*//widget/editor.ui", {
-  path,
-  doc,
-  on,
-  ...props,
-});
+const lastUpdated = doc.updatedAt;
+const isBuffer = doc._.inBuffer;
+
+const Root = styled.div`
+  min-height: max(300px, 80vh);
+  width: 100%;
+  border-radius: 16px;
+  background-color: #f9fbfe;
+  color: #000;
+  display: flex;
+  gap: 4px;
+  border: 1px solid var(--c__border-color);
+
+  --c__border-color: rgb(209, 213, 219);
+
+
+  .c__left {
+    min-height: inherit;
+    border-radius: inherit;
+    width: min(300px, 100%);
+  }
+
+  .c__right {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 0;
+    margin: 1rem 1rem 1rem 0rem;
+    background-color: #fff;
+    padding: 1rem;
+  }
+
+  .c__header {
+    padding: .5rem 1rem;
+    width: 100%;
+    border-radius: 16px;
+    background-color: #fff;
+    border: 1px solid var(--c__border-color);
+    margin-bottom: 1rem;
+
+    input {
+      font-size: 20px;
+      margin: 0 auto;
+      background: none !important;
+      outline: none !important;
+      border: none !important;
+      box-shadow: none !important;
+    }
+  }
+
+  .c__tabs {
+    width: 100%;
+    padding: 0 1rem;
+    display: flex;
+    justify-content: flex-end;
+    gap: 1rem;
+
+    button {
+      outline: none;
+      background: none;
+      padding: 0.5rem 1rem;
+      border-radius: 16px;x
+      cursor: pointer;
+      font-size: 16px;
+      font-weight: 600;
+      color: #000;
+      transition: all 0.3s;
+      transform: translateY(1px);
+      border-radius: 16px 16px 0 0;
+      border: 1px solid transparent;
+      border-bottom: none;
+      
+      &.active {
+        border-color: var(--c__border-color);
+        background-color: #fff;
+      }
+    }
+  }
+
+  .c__editor {
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid var(--c__border-color);
+    border-bottom: none;
+    border-radius: 16px 16px 0 0 ;
+    min-height: 450px;
+  }
+
+  .c__footer {
+    width: 100%;
+    padding: 1rem;
+    border-radius: 0 0 16px 16px;
+    background-color: #fff;
+    border: 1px solid var(--c__border-color);
+    border-top: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  `;
+
+return (
+  <>
+    <Root>
+      <div className="c__left">
+        {/* Pulled this out so selecting a document doesn't refresh the folders too */}
+        {widget("/*__@appAccount__*//widget/editor.uiFolders", props)}
+      </div>
+      <div className="c__right" key={path}>
+        {/* 
+          * We can now swap out the editor below
+        */}
+        {widget("/*__@appAccount__*//widget/editor.ui", {
+          key: path,
+          doc,
+          on,
+          ...props,
+        })}
+        <div className="c__footer">
+          <span>
+            {isBuffer ? "Draft auto-saved " : "Last published "} at{" "}
+            {new Date(lastUpdated).toLocaleTimeString("en-US", {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+            .
+          </span>
+          <div>
+            {widget("/*__@replace:nui__*//widget/Input.Button", {
+              children: "Publish",
+              variant: "success",
+              onClick: on.publish,
+              disabled: !isBuffer,
+            })}
+          </div>
+        </div>
+      </div>
+    </Root>
+  </>
+);
--- apps/create/widget/editor/ui.jsx
+++ apps/create/widget/editor/ui.jsx
@@ -6,151 +6,48 @@ State.init({
   tab: props.tab || "EDIT",
 });
 
-const Root = styled.div`
-  min-height: max(300px, 80vh);
-  width: 100%;
-  border-radius: 16px;
-  background-color: #f9fbfe;
-  color: #000;
-  display: flex;
-  gap: 4px;
-  border: 1px solid var(--c__border-color);
-
-  --c__border-color: rgb(209, 213, 219);
-
-
-  .c__left {
-    min-height: inherit;
-    border-radius: inherit;
-    width: min(300px, 100%);
-  }
-
-  .c__right {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    gap: 0;
-    margin: 1rem 1rem 1rem 0rem;
-    background-color: #fff;
-    padding: 1rem;
-  }
-
-  .c__header {
-    padding: .5rem 1rem;
-    width: 100%;
-    border-radius: 16px;
-    background-color: #fff;
-    border: 1px solid var(--c__border-color);
-    margin-bottom: 1rem;
-
-    input {
-      font-size: 20px;
-      margin: 0 auto;
-      background: none !important;
-      outline: none !important;
-      border: none !important;
-      box-shadow: none !important;
-    }
-  }
-
-  .c__tabs {
-    width: 100%;
-    padding: 0 1rem;
-    display: flex;
-    justify-content: flex-end;
-    gap: 1rem;
-
-    button {
-      outline: none;
-      background: none;
-      padding: 0.5rem 1rem;
-      border-radius: 16px;x
-      cursor: pointer;
-      font-size: 16px;
-      font-weight: 600;
-      color: #000;
-      transition: all 0.3s;
-      transform: translateY(1px);
-      border-radius: 16px 16px 0 0;
-      border: 1px solid transparent;
-      border-bottom: none;
-      
-      &.active {
-        border-color: var(--c__border-color);
-        background-color: #fff;
-      }
-    }
-  }
-
-  .c__editor {
-    width: 100%;
-    padding: 1rem;
-    border: 1px solid var(--c__border-color);
-    border-bottom: none;
-    border-radius: 16px 16px 0 0 ;
-    min-height: 450px;
-  }
-
-  .c__footer {
-    width: 100%;
-    padding: 1rem;
-    border-radius: 0 0 16px 16px;
-    background-color: #fff;
-    border: 1px solid var(--c__border-color);
-    border-top: none;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-`;
-
-const selectedDoc = props.handle["document"].getSelected(props.project);
-const doc = props.handle["document"].get(selectedDoc);
-
-const lastUpdated = doc.updatedAt;
-const isBuffer = doc._.inBuffer;
+// Now this is an isolated editor
+// Can still modify the styles of it using the template
 
 return (
-  <Root>
-    <div className="c__left" key={props.path}>
-      {widget("/*__@appAccount__*//widget/editor.uiFolders", props)}
+  <>
+    <div className="c__header">
+      <input
+        type="text"
+        placeholder="Untitled"
+        defaultValue={props.doc.title}
+        onInput={(e) => on.change("title", e.target.value)}
+        key={props.key}
+      />
     </div>
-    <div className="c__right">
-      <div className="c__header">
-        <input
-          type="text"
-          placeholder="Untitled"
-          defaultValue={props.doc.title}
-          onInput={(e) => on.change("title", e.target.value)}
-          key={props.path}
-        />
-      </div>
-      <div className="c__tabs">
-        <button
-          className={state.tab === "EDIT" ? "active" : ""}
-          onClick={() => State.update({ tab: "EDIT" })}
-        >
-          Write
-        </button>
-        <button
-          className={state.tab === "VIEW" ? "active" : ""}
-          onClick={() => State.update({ tab: "VIEW" })}
-        >
-          View
-        </button>
-      </div>
-      <div className="c__editor">
-        {state.tab === "VIEW" &&
-          widget("openwebbuild.near/widget/Post.Markdown", {
-            text: props.doc.content,
-          })}
+    <div className="c__tabs">
+      <button
+        className={state.tab === "EDIT" ? "active" : ""}
+        onClick={() => State.update({ tab: "EDIT" })}
+      >
+        Write
+      </button>
+      <button
+        className={state.tab === "VIEW" ? "active" : ""}
+        onClick={() => State.update({ tab: "VIEW" })}
+      >
+        View
+      </button>
+    </div>
+    <div className="c__editor">
+      {state.tab === "VIEW" &&
+        widget("openwebbuild.near/widget/Post.Markdown", {
+          text: props.doc.content,
+        })}
 
-        {/* Just hiding the iframe, so that it doesn't reload everything on tab change */}
-        <div
-          className={"w-100 h-100" + (state.tab !== "EDIT" ? " d-none" : "")}
-          key={props.path}
-        >
-          {widget("efiz.near/widget/SimpleMDE", {
+      {/* Just hiding the iframe, so that it doesn't reload everything on tab change */}
+      <div
+        className={"w-100 h-100" + (state.tab !== "EDIT" ? " d-none" : "")}
+        key={props.key}
+      >
+        {widget(
+          "efiz.near/widget/SimpleMDE",
+          {
             onChange: (v) => on.change("content", v),
             data: { content: props.doc.content },
             toolbar: [
@@ -166,27 +63,9 @@ return (
               "mention",
               "reference",
             ],
-          })}
-        </div>
-      </div>
-      <div className="c__footer">
-        <span>
-          {isBuffer ? "Draft auto-saved " : "Last published "} at{" "}
-          {new Date(lastUpdated).toLocaleTimeString("en-US", {
-            hour: "2-digit",
-            minute: "2-digit",
-          })}
-          .
-        </span>
-        <div>
-          {widget("/*__@replace:nui__*//widget/Input.Button", {
-            children: "Publish",
-            variant: "success",
-            onClick: on.publish,
-            disabled: !isBuffer,
-          })}
-        </div>
+          }
+        )}
       </div>
     </div>
-  </Root>
+  </>
 );
