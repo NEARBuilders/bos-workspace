1c182acca271e406f5596d5201ee11f88664c2b8
sekaiking <afaithraf@gmail.com>
2023-08-26 15:33:31 +0800

    editor project box

--- apps/create/widget/editor/uiFolders.jsx
+++ apps/create/widget/editor/uiFolders.jsx
@@ -129,50 +129,64 @@ const renderFolder = (folder) => {
   );
 };
 
+const Project = styled.div`
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  margin-bottom: 2.4rem;
+  padding: 12px;
+
+  a {
+    text-decoration: none;
+    color: #000;
+    transition: opacity 0.2s ease-in-out;
+
+    &:hover {
+      opacity: 0.9;
+    }
+  }
+
+  img {
+    max-width: 80px;
+  }
+`;
+
 const renderProject = (project) => {
   const { title, logo } = project.data;
 
   return (
-    <div>
-      <div className="mb-2">
-        <a
-          onClick={() => {
-            navigate("projects");
-          }}
-          href={Url.construct("#//*__@appAccount__*//widget/home", {
-            page: "projects",
-          })}
-          className="text-decoration-none"
-          style={{
-            cursor: "pointer",
-          }}
-        >
-          <i className="bi bi-arrow-left"></i>
-          Back to projects
-        </a>
-      </div>
+    <Project>
       <a
-        className="d-flex justify-content-center gap-3 align-items-center mb-auto w-100"
-        title="Open project settings"
-        onClick={() => {
-          navigate("manage", { project: projectId });
-        }}
-        href={Url.construct("#//*__@appAccount__*//widget/home", {
-          page: "manage",
-          project: projectId,
+        target="_blank"
+        href={Url.construct("#//*__@appAccount__*//widget/p", {
+          id: projectId,
+          by: context.accountId,
         })}
+        className="d-flex align-items-center mb-4 justify-content-between gap-1"
       >
-        {logo && <img src={logo} alt={title} height={55} width={55} />}
-        <h5
-          className="h6 m-0 flex-fill"
-          style={{
-            lineHeight: 1.5,
-          }}
+        <img src={logo} alt={title} height={40} />
+        <span
+          className="h6 m-0 flex-fill ms-2"
         >
           {title}
-        </h5>
+        </span>
+        <i className="ms-1 bi bi-box-arrow-up-right"></i>
       </a>
-    </div>
+      <div>
+        <a
+          title="Open project settings"
+          onClick={() => {
+            navigate("manage", { project: projectId });
+          }}
+          href={Url.construct("#//*__@appAccount__*//widget/home", {
+            page: "manage",
+            project: projectId,
+          })}
+        >
+          <i className="bi bi-gear"></i>
+          <span className="ms-1">Settings</span>
+        </a>
+      </div>
+    </Project>
   );
 };
 
@@ -304,7 +318,7 @@ const Header = styled.div`
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-rows: 120px auto;
+  grid-template-rows: auto auto;
 `;
 
 return (
--- apps/create/widget/ui/navbar.jsx
+++ apps/create/widget/ui/navbar.jsx
@@ -10,7 +10,6 @@ const update = (k, v) => State.update({ [k]: v });
 
 return (
   <>
-  {/* I'm passing in a template that could be configured in the app provider? */}
     {widget(template ?? "/*__@appAccount__*//widget/templates.ui.navbar.default", {
       open: state.mobileNavbarOpen,
       setOpen: (v) => update("mobileNavbarOpen", v),
