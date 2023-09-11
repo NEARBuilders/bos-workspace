b4c86377c894a949588f0d42a2616ad6f6987a9e
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-09-11 00:58:00 -0400

    clean up

--- apps/voyager/widget/provider.jsx
+++ apps/voyager/widget/provider.jsx
@@ -52,65 +52,14 @@ function setLayout(v) {
 
 const Children = props.Children;
 
-const Wrapper = styled.div`
-  padding: 6px;
-  min-width: 200px;
-  width: 200px;
-  border-radius: 6px;
-  box-shadow: 0 3px 15px -3px rgba(13, 20, 33, 0.13);
-  display: flex;
-  flex-direction: column;
-  border: 1px solid #e8e8eb;
-  background-color: #fff;
-  gap: 1px;
-
-  .menu__item {
-    padding: 3px;
-    display: flex;
-    color: #000;
-    border-radius: 6px;
-    cursor: pointer;
-
-    &:hover {
-      background-color: #eff2f5;
-    }
-  }
-  .menu__item__icon {
-    font-size: 14px;
-    border-radius: 5px;
-    box-shadow: 0 0 0 1px rgba(201, 201, 204, 0.48);
-    background: #fff;
-    color: #000;
-    height: 26px;
-    width: 26px;
-    display: flex;
-    margin-right: 10px;
-    justify-content: center;
-    align-items: center;
-  }
-`;
-
 return (
-  <ContextMenu.Root>
-    <Children
-      setPath={setPath}
-      setHistory={setHistory}
-      setLayout={setLayout}
-      path={state.path}
-      layout={state.layout}
-      goBack={goBack}
-      goForward={goForward}
-    />
-    <ContextMenu.Content sideOffset={5} align="end" asChild>
-      <Wrapper>
-        <ContextMenu.Item
-          className="menu__item"
-          onSelect={() => handler["delete"](passProps["delete"])}
-        >
-          <i className="menu__item__icon bi bi-x-lg" />
-          Delete
-        </ContextMenu.Item>
-      </Wrapper>
-    </ContextMenu.Content>
-  </ContextMenu.Root>
+  <Children
+    setPath={setPath}
+    setHistory={setHistory}
+    setLayout={setLayout}
+    path={state.path}
+    layout={state.layout}
+    goBack={goBack}
+    goForward={goForward}
+  />
 );
