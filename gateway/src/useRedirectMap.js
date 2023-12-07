47a002ca78943cc9a2a9328143a57fe3c605228c
Achraf <54310457+sekaiking@users.noreply.github.com>
2023-12-07 04:44:52 +0100

    Local gateway + Hot Reload using WebSocket (#16)
    * Local gateway + Hot Reload using WebSocket
    
    * prettier
    
    * gateway fixes + cli improvements
    
    * prettier
    
    * remove CSS linter
    
    * rebale
    
    * fixes
    
    * fix gateway message
    
    * prettier
    
    * fix gateway router
    
    * add authentication & navbar
    
    * build script log
    
    * prettier
    
    * remove markdown linter
    
    * fix
    
    * change tabs width to 2
    
    * remove unused css
    
    * Update VM + remove sanitize
    
    * group gateway home widgets by account
    
    * fix: sanitize url
    
    * prettier
    
    ---------
    
    Co-authored-by: Elliot Braem <16282460+elliotBraem@users.noreply.github.com>

---
+++ gateway/src/useRedirectMap.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import { flags } from "../config/flags.js";
+
+function useRedirectMap() {
+  const [devJson, setDevJson] = useState({
+    components: {},
+    data: {},
+  });
+  const [uWebSocket, setUWebSocket] = useState(flags.enableHotReload);
+
+  useEffect(() => {
+    if (uWebSocket) {
+      const socket = io(flags.bosLoaderWs, {
+        reconnectionAttempts: 1, // Limit reconnection attempts
+      });
+
+      socket.on("fileChange", (d) => {
+        console.log("File change detected via WebSocket", d);
+        setDevJson(d);
+      });
+
+      socket.on("connect_error", () => {
+        console.warning("WebSocket connection error. Switching to HTTP.");
+        setUWebSocket(false);
+        socket.disconnect();
+      });
+
+      return () => {
+        socket.disconnect();
+      };
+    } else {
+      fetch(flags.bosLoaderUrl)
+        .then((r) => r.json())
+        .then((d) => {
+          console.log("File change detected via HTTP", d);
+          setDevJson(d);
+        })
+        .catch((error) => {
+          console.error("HTTP request failed:", error);
+        });
+    }
+  }, [uWebSocket]);
+
+  return devJson;
+}
+
+export default useRedirectMap;
