07029422cc221a18508b83ac25b39c67ae2d88fa
sekaiking <afaithraf@gmail.com>
2023-12-07 23:48:42 +0100

    fix

---
+++ gateway/dist/7707.1661a88c65928c01bb81.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbos_workspace_gateway=self.webpackChunkbos_workspace_gateway||[]).push([[7707],{77707:(a,e,s)=>{s.d(e,{take:()=>k.q,takeUntil:()=>t.R});var k=s(74727),t=s(83505)}}]);
