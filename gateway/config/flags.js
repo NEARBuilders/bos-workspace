03e49c7599ff5e2dd265c3d22072fa7f49322c9f
sekaiking <afaithraf@gmail.com>
2023-12-11 06:15:52 +0100

    format

--- gateway/config/flags.js
+++ gateway/config/flags.js
@@ -1,10 +1,15 @@
-const envConfig = document.getElementById('env-config').textContent;
+const envConfig = document.getElementById("env-config").textContent;
 const config = JSON.parse(envConfig);
 
 export const flags = {
-  bosLoaderUrl: process.env.BOS_LOADER_URL || config.bosLoaderUrl || "http://127.0.0.1:4040",
-  bosLoaderWs: process.env.BOS_LOADER_WS || config.bosLoaderWs || "ws://127.0.0.1:4040",
-  enableHotReload: process.env.ENABLE_HOT_RELOAD ?? config.enableHotReload ?? true,
+  bosLoaderUrl:
+    process.env.BOS_LOADER_URL ||
+    config.bosLoaderUrl ||
+    "http://127.0.0.1:4040",
+  bosLoaderWs:
+    process.env.BOS_LOADER_WS || config.bosLoaderWs || "ws://127.0.0.1:4040",
+  enableHotReload:
+    process.env.ENABLE_HOT_RELOAD ?? config.enableHotReload ?? true,
 };
 
 console.log("flags", flags);
