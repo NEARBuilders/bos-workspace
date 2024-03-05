cdf281dc9a9eb524997d96fc9e093875fe715c71
Seth Feibus <sefeibus@gmail.com>
2024-03-05 16:40:44 -0500

    Stub manifest creation & registry upload command

--- gateway/config/webpack.production.js
+++ gateway/config/webpack.production.js
@@ -20,6 +20,16 @@ module.exports = () => {
         //   //   },
         // },
         {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+        {
           test: /\.(scss|css)$/,
           use: [
             {
