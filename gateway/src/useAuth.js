6039b1e1348e3cd6d3694fca1be56230d25c0791
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-04-09 23:57:45 -0400

    update gateway

--- gateway/src/useAuth.js
+++ gateway/src/useAuth.js
@@ -71,7 +71,7 @@ export function useAuth() {
         },
         config: {
           defaultFinality: undefined,
-          nodeUrl: NetworkId === "mainnet" ? "https://near.lava.build" : "near-testnet.lava.build",
+          nodeUrl: NetworkId === "mainnet" ? "https://near.lava.build" : "https://near-testnet.lava.build",
         },
       });
   }, [initNear]);
