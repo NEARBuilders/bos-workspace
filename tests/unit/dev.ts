99975f7424b0da235a6749896d90f142cd7a0870
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-04-26 10:50:21 -0400

    dev tests

--- tests/unit/dev.ts
+++ tests/unit/dev.ts
@@ -1,10 +1,12 @@
-import express from 'express';
-import http from 'http';
-import supertest from 'supertest';
-import fetchMock from 'jest-fetch-mock';
-import { DEFAULT_CONFIG } from '@/lib/config';
-import { dev, DevOptions } from '@/lib/dev'; // Update with the correct path to your server file
-import { LogLevel, Logger } from "@/lib/logger";
+import { buildApp } from "@/lib/build";
+import { DEFAULT_CONFIG, loadConfig } from "@/lib/config";
+import { dev, DevOptions } from "@/lib/dev";
+import { Logger, LogLevel } from "@/lib/logger";
+import { startDevServer } from "@/lib/server";
+import { startSocket } from "@/lib/socket";
+import { startFileWatcher } from "@/lib/watcher";
+import http from "http";
+import { Server as IoServer } from "socket.io";
 
 import { vol } from 'memfs';
 jest.mock('fs', () => require('memfs').fs);
@@ -12,19 +14,69 @@ jest.mock('fs/promises', () => require('memfs').fs.promises);
 
 const unmockedLog = global.log;
 
-// Mock fetch
-fetchMock.enableMocks();
-
 const app_example_1 = {
-  "./bos-loader.json": JSON.stringify({"components":{"test.testnet/widget/home":{"code":"retrun <p>hello world</p>"}},"data":{}}),
+  "./build/bos-loader.json": JSON.stringify({ "components": { "test.testnet/widget/home": { "code": "return <p>hello world</p>" } }, "data": {} }),
 };
 
-// Mock express app
-const app = express();
+jest.mock("@/lib/config");
+jest.mock("@/lib/server");
+jest.mock("@/lib/socket");
+jest.mock("@/lib/watcher");
+jest.mock("@/lib/build")
 
-// Mock express server
-const server = http.createServer(app);
+describe("dev", () => {
+  const mockSrc = "/app_example_1";
+  const mockOpts: DevOptions = {};
+  const mockConfig = DEFAULT_CONFIG;
 
-describe('dev', () => {
-  
+  beforeEach(() => {
+    vol.reset();
+    vol.fromJSON(app_example_1, mockSrc);
+    global.log = new Logger(LogLevel.DEV);
+
+    (loadConfig as jest.MockedFunction<typeof loadConfig>).mockResolvedValue(mockConfig);
+    (startDevServer as jest.MockedFunction<typeof startDevServer>).mockReturnValue({} as http.Server);
+    (startSocket as jest.MockedFunction<typeof startSocket>).mockReturnValue({} as IoServer);
+    (buildApp as jest.MockedFunction<typeof buildApp>).mockReturnValue({} as Promise<any>);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    global.log = unmockedLog;
+  });
+
+  it("should call loadConfig with src and opts.network", async () => {
+    await dev(mockSrc, mockOpts);
+    expect(loadConfig).toHaveBeenCalledWith(mockSrc, mockOpts.network);
+  });
+
+  it("should call generateApp with src, dist, config, opts, and devJsonPath", async () => {
+    await dev(mockSrc, mockOpts);
+    const mockDevJsonPath = `${mockSrc}/build/bos-loader.json`;
+    expect(startDevServer).toHaveBeenCalledWith(mockDevJsonPath, mockOpts);
+  });
+
+  it("should start the socket server if hot reload is enabled", async () => {
+    const mockHotOpts: DevOptions = { NoHot: false };
+    await dev(mockSrc, mockHotOpts);
+    expect(startSocket).toHaveBeenCalled();
+  });
+
+  it("should not start the socket server if hot reload is disabled", async () => {
+    const mockNoHotOpts: DevOptions = { NoHot: true };
+    await dev(mockSrc, mockNoHotOpts);
+    expect(startSocket).not.toHaveBeenCalled();
+  });
+
+  it("should call startFileWatcher with correct watch paths", async () => {
+    await dev(mockSrc, mockOpts);
+    const expectedWatchPaths = [
+      `${mockSrc}/widget/**/*`,
+      `${mockSrc}/module/**/*`,
+      `${mockSrc}/ipfs/**/*`,
+      `${mockSrc}/bos.config.json`,
+      `${mockSrc}/aliases.json`,
+    ];
+    expect(startFileWatcher).toHaveBeenCalledWith(expectedWatchPaths, expect.any(Function));
+  });
 });
