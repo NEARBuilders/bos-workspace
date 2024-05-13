25a864f9b424eddca41c69d79619c104757e74ea
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-05-12 22:33:40 -0400

    handle replacements

---
+++ lib/gateway.ts
@@ -0,0 +1,49 @@
+import { DevOptions } from "./dev";
+
+function renderAttribute(name, value) {
+  return value !== undefined ? `${name}="${value}"` : "";
+}
+
+function htmlStringify(json) {
+  return JSON.stringify(json)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
+
+export const handleReplacements = (html: string, opts: DevOptions): string => {
+  const envConfig = JSON.stringify({
+    bosLoaderWs: `ws://127.0.0.1:${opts.port}`,
+    bosLoaderUrl: `http://127.0.0.1:${opts.port}/api/loader`,
+    enableHotReload: opts.NoHot ? false : true,
+    network: opts.network,
+  });
+
+  return normalizeHtml(injectHTML(html, {
+    ENV_CONFIG: {
+      pattern: "%ENV_CONFIG%",
+      replacement: envConfig
+    },
+    OTHER: {
+      pattern: /<near-social-viewer><\/near-social-viewer>/g,
+      replacement: true
+        ? `<near-social-viewer
+        ${renderAttribute("rpc", `http://127.0.0.1:${opts.port}/api/proxy-rpc`)}></near-social-viewer>`
+        : "<near-social-viewer></near-social-viewer>",
+    }
+  }));
+}
+
+function injectHTML(html: string, injections: Record<string, any>) {
+  Object.keys(injections).forEach((key) => {
+    const { pattern, replacement } = injections[key];
+    html = html.replace(pattern, replacement);
+  });
+  return html;
+};
+
+function normalizeHtml(html) {
+  return html.replace(/\s+/g, ' ').trim();
+}
